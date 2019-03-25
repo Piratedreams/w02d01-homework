3972118fa3e9fb1f2c626a0de874c64b173af9f1
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-03-25 16:13:47 -0600

    data types

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log('running');
+
+// A light switch that can be either on or off.
+
+// A boolean value since it is binary for its settings. Either on or off, true or false.
+
+// A user 's email address.
+
+    // A string because you're allowed to have Characters and whole sentences.
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+
+    // An object because those are the characteristics of the object.
+
+// A list of student names from our class.
+
+    // An array for listing different values.
+
+
+// A list of student names from our class, each with a location.
+
+    // Using an array to have the students and having the students that are objects that give them details.
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+
+    // An array of students with objects again just add the strings of their location and of their favorite TV show within the objects.
+
+
+
