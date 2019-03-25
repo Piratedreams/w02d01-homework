e8073e3e4d82e8ee4c54a7471471c60f39363d56
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-03-25 16:40:37 -0600

    object-ception

--- js/app.js
+++ js/app.js
@@ -40,37 +40,58 @@ console.log('running');
 
 // console.log(ben.hobby);
 
-const crazyObject = {
-    taco: [{
-            meat: 'steak',
-            cheese: ['panela', 'queso', 'chihuahua']
-        },
-        {
-            meat: 'chicken',
-            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
-        },
-    ],
-    larry: {
-        nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-        quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-        characters: [{
-                name: "Jeff",
-                occupation: "manager"
-            },
-            {
-                name: "funkhauser",
-                occupation: "tv dude"
-            },
-            {
-                name: "susie",
-                occupation: "jeffs wife",
-                favourtieHobby: "Swearing at Larry and Jeff"
-            },
-        ]
+// const crazyObject = {
+//     taco: [{
+//             meat: 'steak',
+//             cheese: ['panela', 'queso', 'chihuahua']
+//         },
+//         {
+//             meat: 'chicken',
+//             salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+//         },
+//     ],
+//     larry: {
+//         nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//         quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//         characters: [{
+//                 name: "Jeff",
+//                 occupation: "manager"
+//             },
+//             {
+//                 name: "funkhauser",
+//                 occupation: "tv dude"
+//             },
+//             {
+//                 name: "susie",
+//                 occupation: "jeffs wife",
+//                 favourtieHobby: "Swearing at Larry and Jeff"
+//             },
+//         ]
+//     }
+// }
+// console.log(crazyObject.taco[1].salsa[5]);
+// console.log(crazyObject.larry.quotes[0]);
+// console.log(crazyObject.larry.characters[2].favourtieHobby);
+// console.log(crazyObject.larry.nicknames[1]);
+// console.log(crazyObject.larry.characters[1]);
+
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-console.log(crazyObject.taco[1].salsa[5]);
-console.log(crazyObject.larry.quotes[0]);
-console.log(crazyObject.larry.characters[2].favourtieHobby);
-console.log(crazyObject.larry.nicknames[1]);
-console.log(crazyObject.larry.characters[1]);
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
